fb5b19ca3a8c7603153dd1bc6ff9df6e6153b61b
Cameron Munroe <cmunroe@cameronmunroe.com>
2014-09-14 08:04:54 -0700

    Live and Outage Fixes
    * Fixed live.php
    * Fixed Mobile issue with outages page.
    * Suggested better cache and refresh times in the config file
    * Love

--- outages.php
+++ outages.php
@@ -10,37 +10,37 @@ $sTable ='
 			<tr>
 				<th id="name">Name</th>
 				<th id="name">Host</th>
-				<th id="name">Type</th>
-				<th id="name">Down</th>
-				<th id="name">Up</th>
-				<th id="name">Total</th>
+				<th id="type">Type</th>
+				<th id="type">Down</th>
+				<th id="type">Up</th>
+				<th id="uptime">Total</th>
 			</tr>
 			</thead>
 			<tbody>';
 			
 if ($name == null && $host == null) {
 	foreach($outages as $outage) {
-		$sTable .='<tr><td><a href="outages.php?name=' . $outage['name'] . '">'. $outage['name'] . '</a></td><td><a href="outages.php?host=' . $outage['host'] . '">'. $outage['host'] . '</a></td><td>' . $outage['type'] . '</td><td>' . date("H:i:s -- d M Y", $outage['down']) . '</td><td>' . date("H:i:s -- d M Y", $outage['upagain']) . '</td><td>' . number_format((($outage['upagain'] - $outage['down']) / 60), 1, '.', '') . ' Minutes</td></tr>';
+		$sTable .='<tr><td><a href="outages.php?name=' . $outage['name'] . '">'. $outage['name'] . '</a></td><td><a href="outages.php?host=' . $outage['host'] . '">'. $outage['host'] . '</a></td><td>' . $outage['type'] . '</td><td>' . date("H:i | d M Y", $outage['down']) . '</td><td>' . date("H:i | d M Y", $outage['upagain']) . '</td><td>' . number_format((($outage['upagain'] - $outage['down']) / 60), 0, '.', '') . ' Min</td></tr>';
 	}
 }
 elseif($name != null && $host == null) {
 	foreach($outages as $outage) {
 		if ($name == $outage['name']) {
-			$sTable .='<tr><td><a href="outages.php?name=' . $outage['name'] . '">'. $outage['name'] . '</a></td><td><a href="outages.php?host=' . $outage['host'] . '">'. $outage['host'] . '</a></td><td>' . $outage['type'] . '</td><td>' . date("H:i:s -- d M Y", $outage['down']) . '</td><td>' . date("H:i:s -- d M Y", $outage['upagain']) . '</td><td>' . number_format((($outage['upagain'] - $outage['down']) / 60), 1, '.', '') . ' Minutes</td></tr>';
+			$sTable .='<tr><td><a href="outages.php?name=' . $outage['name'] . '">'. $outage['name'] . '</a></td><td><a href="outages.php?host=' . $outage['host'] . '">'. $outage['host'] . '</a></td><td>' . $outage['type'] . '</td><td>' . date("H:i | d M Y", $outage['down']) . '</td><td>' . date("H:i | d M Y", $outage['upagain']) . '</td><td>' . number_format((($outage['upagain'] - $outage['down']) / 60), 0, '.', '') . ' Min</td></tr>';
 		}
 	}
 }
 elseif($name == null && $host != null) {
 	foreach($outages as $outage) {
 		if ($host == $outage['host']) {
-			$sTable .='<tr><td><a href="outages.php?name=' . $outage['name'] . '">'. $outage['name'] . '</a></td><td><a href="outages.php?host=' . $outage['host'] . '">'. $outage['host'] . '</a></td><td>' . $outage['type'] . '</td><td>' . date("H:i:s -- d M Y", $outage['down']) . '</td><td>' . date("H:i:s -- d M Y", $outage['upagain']) . '</td><td>' . number_format((($outage['upagain'] - $outage['down']) / 60), 1, '.', '') . ' Minutes</td></tr>';
+			$sTable .='<tr><td><a href="outages.php?name=' . $outage['name'] . '">'. $outage['name'] . '</a></td><td><a href="outages.php?host=' . $outage['host'] . '">'. $outage['host'] . '</a></td><td>' . $outage['type'] . '</td><td>' . date("H:i | d M Y", $outage['down']) . '</td><td>' . date("H:i | d M Y", $outage['upagain']) . '</td><td>' . number_format((($outage['upagain'] - $outage['down']) / 60), 0, '.', '') . ' Min</td></tr>';
 		}
 	}
 }
 elseif($name != null && $host != null) {
 	foreach($outages as $outage) {
 		if ($host == $outage['host'] && $host == $outage['host']) {
-			$sTable .='<tr><td><a href="outages.php?name=' . $outage['name'] . '">'. $outage['name'] . '</a></td><td><a href="outages.php?host=' . $outage['host'] . '">'. $outage['host'] . '</a></td><td>' . $outage['type'] . '</td><td>' . date("H:i:s -- d M Y", $outage['down']) . '</td><td>' . date("H:i:s -- d M Y", $outage['upagain']) . '</td><td>' . number_format((($outage['upagain'] - $outage['down']) / 60), 1, '.', '') . ' Minutes</td></tr>';
+			$sTable .='<tr><td><a href="outages.php?name=' . $outage['name'] . '">'. $outage['name'] . '</a></td><td><a href="outages.php?host=' . $outage['host'] . '">'. $outage['host'] . '</a></td><td>' . $outage['type'] . '</td><td>' . date("H:i | d M Y", $outage['down']) . '</td><td>' . date("H:i | d M Y", $outage['upagain']) . '</td><td>' . number_format((($outage['upagain'] - $outage['down']) / 60), 0, '.', '') . ' Min</td></tr>';
 		}
 	}
 }
