80078ed7e1d22125e483bf7e5d151600868826ec
Michael Ojeda <mojeda92@gmail.com>
2015-01-06 14:57:32 -0800

    Removed Personal Piwik Tracker
    Personal Piwik analytics tracker slipped in by accident.

--- themes/pingstatus/footer.php
+++ themes/pingstatus/footer.php
@@ -14,25 +14,9 @@
     });
     </script>
 
-    <!-- Piwik -->
-	<script type="text/javascript">
-	  var _paq = _paq || [];
-	  _paq.push(['trackPageView']);
-	  _paq.push(['enableLinkTracking']);
-	  (function() {
-	    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://apps.mojeda.net/analytics/";
-	    _paq.push(['setTrackerUrl', u+'piwik.php']);
-	    _paq.push(['setSiteId', 3]);
-	    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
-	    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-	  })();
-	</script>
-	<noscript><p><img src="http://apps.mojeda.net/analytics/piwik.php?idsite=3" style="border:0;" alt="" /></p></noscript>
-	<!-- End Piwik Code -->
-
-	<script type="text/javascript">
-		window.setTimeout(function(){ document.location.reload(); }, <?php echo $refresh; ?>);
-	</script>
+    <script type="text/javascript">
+        window.setTimeout(function(){ document.location.reload(); }, <?php echo $refresh; ?>);
+    </script>
 
   </body>
 </html>
