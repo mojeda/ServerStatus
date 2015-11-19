aaee89d1370dfafe49511e507b3e11548cf88c10
Kryptos <kryptos@ukblabberbox.co.uk>
2015-11-19 01:40:54 +0000

    Added .gitignore for .idea/
    Refactored the configuration file to use PDO rather than mysql_* which has been deprecated
    for a long time now.

--- includes/config.php
+++ includes/config.php
@@ -6,9 +6,23 @@ $pass = '';
 $data = '';
 $sSetting['refresh'] = "10000";
 
-mysql_connect($host, $user, $pass) or die(mysql_error());
-mysql_select_db($data) or die(mysql_error());
 //Template options: "default" and "dark"
 $template = "./templates/default/";
 $index = $template . "index.php";
-?>
+
+
+class DB extends PDO
+{
+    public function __construct($db)
+    {
+        $dsn = sprintf('mysql:dbname=%s;host%s', $db['data'], $db['host']);
+        try {
+            return new PDO($dsn, $db['user'], $db['pass'], PDO::FETCH_OBJ);
+        } catch (PDOException $e) {
+            die("Database error: " . $e->getMessage());
+        }
+
+    }
+}
+
+$db = new DB(['host' => $host, 'user' => $user, 'pass' => $pass, 'data' => $data]);
